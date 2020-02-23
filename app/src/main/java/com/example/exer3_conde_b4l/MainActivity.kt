27a7379d562bb21c6fbb6ad2adca7594b708d2b0
Allyza <allyzaconde>
2020-02-24 00:01:34 +0800

    feat: add update nickname

--- app/src/main/java/com/example/exer3_conde_b4l/MainActivity.kt
+++ app/src/main/java/com/example/exer3_conde_b4l/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.exer3_conde_b4l
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button_done).setOnClickListener {
+            updateNickname(it)
+        }
+    }
+
+    fun updateNickname(view: View) {
+        val nicknameEditText = findViewById<EditText>(R.id.edit_nickname)
+        val nicknameTextView = findViewById<TextView>(R.id.view_nickname)
+
+        nicknameTextView.text = nicknameEditText.text
+
+        nicknameEditText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+    }
+
+    fun changeNickname(view: View) {
+        val nicknameEditText = findViewById<EditText>(R.id.edit_nickname)
+        val nicknameTextView = findViewById<TextView>(R.id.view_nickname)
+
+        nicknameEditText.visibility = View.VISIBLE
+        view.visibility = View.VISIBLE
+        nicknameTextView.visibility = View.GONE
     }
 }
