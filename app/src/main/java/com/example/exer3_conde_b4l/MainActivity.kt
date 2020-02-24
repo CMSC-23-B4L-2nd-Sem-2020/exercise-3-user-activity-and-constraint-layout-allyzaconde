32bebd118bceb3cafbe82404f3ababc023364d7c
Allyza <allyzaconde>
2020-02-25 02:32:25 +0800

    chore: setting up for light's out game

--- app/src/main/java/com/example/exer3_conde_b4l/MainActivity.kt
+++ app/src/main/java/com/example/exer3_conde_b4l/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.exer3_conde_b4l
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,17 +26,32 @@ class MainActivity : AppCompatActivity() {
 
         nicknameTextView.text = nicknameEditText.text
 
-        nicknameEditText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+            nicknameEditText.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameTextView.visibility = View.VISIBLE
+
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
     fun changeNickname(view: View) {
         val nicknameEditText = findViewById<EditText>(R.id.edit_nickname)
         val nicknameTextView = findViewById<TextView>(R.id.view_nickname)
+        val nicknameButton = findViewById<Button>(R.id.button_done)
 
         nicknameEditText.visibility = View.VISIBLE
-        view.visibility = View.VISIBLE
+        nicknameButton.visibility = View.VISIBLE
         nicknameTextView.visibility = View.GONE
     }
+
+    fun closeLights(view: View){
+
+    }
+
+    fun retry(view: View){
+
+    }
+
+
 }
