e3375ee1d01ecca3b535945fe9cc790868e72f1f
Allyza <allyzaconde>
2020-02-26 04:18:03 +0800

    feat: add lights out game

--- app/src/main/java/com/example/exer3_conde_b4l/MainActivity.kt
+++ app/src/main/java/com/example/exer3_conde_b4l/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.exer3_conde_b4l
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -9,8 +11,13 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+    private var moveCount:Int = 0
+    private lateinit var moveCountView:TextView
+    private val array: IntArray = intArrayOf(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +25,9 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button_done).setOnClickListener {
             updateNickname(it)
         }
+        moveCountView = findViewById(R.id.view_count)
+        setListeners()
+        check()
     }
 
     fun updateNickname(view: View) {
@@ -45,13 +55,128 @@ class MainActivity : AppCompatActivity() {
         nicknameTextView.visibility = View.GONE
     }
 
-    fun closeLights(view: View){
+    private fun getId(int: Int): Int{
+        val list: List<Int> = listOf(
+            R.id.box_1,
+            R.id.box_2,
+            R.id.box_3,
+            R.id.box_4,
+            R.id.box_5,
+            R.id.box_6,
+            R.id.box_7,
+            R.id.box_8,
+            R.id.box_9,
+            R.id.box_10,
+            R.id.box_11,
+            R.id.box_12,
+            R.id.box_13,
+            R.id.box_14,
+            R.id.box_15,
+            R.id.box_16,
+            R.id.box_17,
+            R.id.box_18,
+            R.id.box_19,
+            R.id.box_20,
+            R.id.box_21,
+            R.id.box_22,
+            R.id.box_23,
+            R.id.box_24,
+            R.id.box_25
+        )
+        return list[int]
+    }
 
+    private fun setListeners(){
+        for (item in (0..24)){
+            findViewById<TextView>(getId(item)).setOnClickListener{
+                closeLights(it)
+            }
+        }
+    }
+
+    private fun closeLights(view: View){
+        if(moveCount == 0){
+            val show = findViewById<TextView>(R.id.view_count)
+            show.visibility = View.VISIBLE
+        }
+        moveCount++
+        moveCountView.text = moveCount.toString()
+
+        for(item in (0..24)){
+            if(getId(item) == view.id){
+                checkLights(item)
+            }
+        }
+    }
+
+    private fun checkLights(int:Int){
+        if(array[int] == 0){
+            array[int] = 1
+            findViewById<TextView>(getId(int)).setBackgroundColor(Color.DKGRAY)
+        }else{
+            array[int] = 0
+            findViewById<TextView>(getId(int)).setBackgroundColor(Color.CYAN)
+        }
+
+        if(int<24){
+            if(array[int+1] == 0 && ((int+1)%5)!=0){
+                array[int+1] = 1
+                findViewById<TextView>(getId(int+1)).setBackgroundColor(Color.DKGRAY)
+            }else if(array[int+1] == 1 && ((int+1)%5)!=0){
+                array[int+1] = 0
+                findViewById<TextView>(getId(int+1)).setBackgroundColor(Color.CYAN)
+            }
+        }
+
+        if(int>0){
+            if(array[int-1] == 0 && (int%5)!=0){
+                array[int-1] = 1
+                findViewById<TextView>(getId(int-1)).setBackgroundColor(Color.DKGRAY)
+            }else if(array[int-1] == 1 && (int%5)!=0){
+                array[int-1] = 0
+                findViewById<TextView>(getId(int-1)).setBackgroundColor(Color.CYAN)
+            }
+        }
+
+        if(int>4){
+            if(array[int-5] == 0){
+                array[int-5] = 1
+                findViewById<TextView>(getId(int-5)).setBackgroundColor(Color.DKGRAY)
+            }else{
+                array[int-5] = 0
+                findViewById<TextView>(getId(int-5)).setBackgroundColor(Color.CYAN)
+            }
+        }
+
+        if(int<20){
+            if(array[int+5] == 0){
+                array[int+5] = 1
+                findViewById<TextView>(getId(int+5)).setBackgroundColor(Color.DKGRAY)
+            }else{
+                array[int+5] = 0
+                findViewById<TextView>(getId(int+5)).setBackgroundColor(Color.CYAN)
+            }
+        }
     }
 
     fun retry(view: View){
-
+        moveCount = 0
+        val show = findViewById<TextView>(R.id.view_count)
+        show.visibility = View.GONE
+        for(item in (0..24)){
+            findViewById<TextView>(getId(item)).setBackgroundColor(Color.CYAN)
+            array[item] = 0
+        }
     }
 
+    @SuppressLint("ResourceType")
+    private fun check(){
+        for(i in (0..24)){
+            if(array[i]==0){
+                return
+            }
+        }
+        moveCountView = findViewById(R.string.winner)
+    }
 
 }
